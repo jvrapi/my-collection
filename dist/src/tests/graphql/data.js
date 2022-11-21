5a9e65a9b8f37c7cf115afaa9b3340ad3c0e7769
jvrapi <joaooviitorr@hotmail.com>
2022-11-21 08:27:07 -0300

    removed dist folder

--- dist/src/tests/graphql/data.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createUserVariables = void 0;
-exports.createUserVariables = {
-    email: 'zalari@fozorat.bd',
-    name: 'Gabriel Figueroa',
-    password: '2exyrQcg',
-    username: 'QGULNpCoQD'
-};
